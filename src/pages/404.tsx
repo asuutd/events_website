09a199be454c6e42a75c90867a398e073f20bfc3
Pelps <oluwapelps@gmail.com>
2022-10-03 17:24:33 -0500

    Change ymbol to apostrophe

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 
 const ErrorPage = () => {
 	return (
-		<div className="flex flex-col justify-center min-h-[66vh]">
-			<h2 className="text-secondary text-7xl text-center">:(</h2>
-			<p>Life is hard as a sole developer. Don't make it harder</p>
-			<Link href="/" className="underline text-center">
-				<a>Go home</a>
+		<div className="flex flex-col justify-center min-h-[66vh] gap-3">
+			<h2 className="text-secondary text-7xl text-center">&#128517;</h2>
+			<p className="text-center font-semibold text-primary">
+				Life is hard as a solo developer. Don&apos;t make it harder
+			</p>
+			<Link href="/">
+				<a className="underline text-center">Go home</a>
 			</Link>
 		</div>
 	);
